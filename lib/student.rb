1107c91aacabf2650cfbc6cb93a7d22d8fee0383
Icelle <icelleku@gmail.com>
2013-11-30 18:43:01 -0500

    add Comparable to Student, so students can be sorted

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,7 @@
 require_relative './final_grade.rb'
 
 class Student
+  include Comparable
   attr_reader :first_name, :last_name, :assignment_grades, :final_grade
 
   # @param [String] first_name - student's first name
@@ -12,5 +13,9 @@ class Student
     @last_name         = last_name
     @assignment_grades = grades
     @final_grade = FinalGrade.new(self)
+  end
+
+  def <=>(student2)
+    (@last_name + @first_name) <=> (student2.last_name + student2.first_name)
   end
 end
